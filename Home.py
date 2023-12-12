11cf4de6a83b46dfcae0d7e67d75803d3b09ea47
Andre-M-Souza <andre_m_souza@outlook.com>
2023-12-12 17:00:37 -0300

    update

--- Home.py
+++ Home.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_lightweight_charts import renderLightweightCharts
+import streamlit_lightweight_charts.dataSamples as data
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 from matplotlib import colors
@@ -269,3 +271,72 @@ with st.container(border=True):
 
             st.markdown('Absenteísmo')
             st.metric('registros :-1:', value='54', delta='52')
+
+            priceVolumeChartOptions = {
+                "height": 300,
+                "rightPriceScale": {
+                    "scaleMargins": {
+                        "top": 0.2,
+                        "bottom": 0.25,
+                    },
+                    "borderVisible": False,
+                },
+                "overlayPriceScales": {
+                    "scaleMargins": {
+                        "top": 0.7,
+                        "bottom": 0,
+                    }
+                },
+                "layout": {
+                    "background": {
+                        "type": 'solid',
+                        "color": '#131722'
+                    },
+                    "textColor": '#d1d4dc',
+                },
+                "grid": {
+                    "vertLines": {
+                        "color": 'rgba(42, 46, 57, 0)',
+                    },
+                    "horzLines": {
+                        "color": 'rgba(42, 46, 57, 0.6)',
+                    }
+                }
+            }
+
+            priceVolumeSeries = [
+                {
+                    "type": 'Area',
+                    "data": data.priceVolumeSeriesArea,
+                    "options": {
+                        "topColor": 'rgba(38,198,218, 0.56)',
+                        "bottomColor": 'rgba(38,198,218, 0.04)',
+                        "lineColor": 'rgba(38,198,218, 1)',
+                        "lineWidth": 2,
+                    }
+                },
+                {
+                    "type": 'Histogram',
+                    "data": data.priceVolumeSeriesHistogram,
+                    "options": {
+                        "color": '#26a69a',
+                        "priceFormat": {
+                            "type": 'volume',
+                        },
+                        "priceScaleId": ""  # set as an overlay setting,
+                    },
+                    "priceScale": {
+                        "scaleMargins": {
+                            "top": 0.7,
+                            "bottom": 0,
+                        }
+                    }
+                }
+            ]
+
+            renderLightweightCharts([
+                {
+                    "chart": priceVolumeChartOptions,
+                    "series": priceVolumeSeries
+                }
+            ], 'priceAndVolume')
